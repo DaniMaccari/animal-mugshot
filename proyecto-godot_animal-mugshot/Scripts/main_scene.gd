82010a124d500eaa2fd8ac14ba7e9366201e93ee
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2025-02-08 20:56:06 +0100

    Add dress characters randomly

--- proyecto-godot_animal-mugshot/Scripts/main_scene.gd
+++ proyecto-godot_animal-mugshot/Scripts/main_scene.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 const suspects_num : int = 3
+const max_item : int = 1
 var all_mugshots : Array = []
+var num_target : int
 
-# Array safe body parts [ head, face,
+# Array safe body parts [ head, face, back, xray
 var safe_items : Array = [-1, -1, -1, -1]
 @onready var mugshot_positions: Node2D = $Mugshot_Positions
 const MUGSHOT_SCENE: PackedScene = preload("res://Scenes/mugshot.tscn")
@@ -16,7 +18,16 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+func Update_Props() -> void:
+	for i : int in range(safe_items.size()):
+		safe_items[i] = randi_range(0, 1)
+		
+
 func Update_Mugshots() -> void:
+	
+	# select new bad
+	num_target = randi_range(0, suspects_num)
 	
 	# clean old characters
 	for mugshot in all_mugshots:
@@ -31,11 +42,27 @@ func Update_Mugshots() -> void:
 			print("Advertencia: No hay suficientes posiciones Marker2D")
 			break
 		
-		var new_mugshot = MUGSHOT_SCENE.instantiate()
-		$Mugshots_Canvas.add_child(new_mugshot)
+		var new_character = MUGSHOT_SCENE.instantiate()
+		if i == num_target:
+			new_character.set_target()
+		new_character.Dress_Character()
+		#Dress_Character(new_mugshot)
+		$Mugshots_Canvas.add_child(new_character)
 		
 		var marker: Marker2D = mugshot_positions.get_child(i)
-		new_mugshot.position = marker.position
-		new_mugshot.set_grid_position(marker.position)
+		new_character.position = marker.position
+		new_character.set_grid_position(marker.position)
 		
-		all_mugshots.append(new_mugshot)
+		all_mugshots.append(new_character)
+
+func Dress_Character(character : PackedScene) -> void:
+	
+	#if character.is_target:
+		##DEBUG
+		#if safe_items[0] == 0:
+			#character.
+	pass
+
+func _input(event : InputEvent) -> void:
+	if event.is_action_pressed("ui_down"): #DEBUG
+		Update_Props()
--- proyecto-godot_animal-mugshot/Scripts/mugshot.gd
+++ proyecto-godot_animal-mugshot/Scripts/mugshot.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
+const BODY_INDEX : int = 1
 var my_position : Vector2
 var of_set : Vector2 = Vector2( 0, 0)
 var dragging : bool = false
 
-var is_bad : bool = false
+var is_target : bool = false
 var in_jail : bool = false
 var front_side : bool = false
 
@@ -19,15 +20,40 @@ func _process(delta: float) -> void:
 		position = lerp(position, get_global_mouse_position() - of_set, delta *15)
 	else:
 		position = my_position
-	
 
 func set_grid_position( grid_position : Vector2) -> void:
 	my_position = grid_position
 
+func set_target():
+	is_target = true
+
+func Dress_Character():
+	
+	# parte delantera
+	for i : int in range($Body_Front.get_child_count()):
+		print("body part ", i)
+		var new_item = randi_range(-1, $Body_Front.get_child(i).get_child_count() -1)
+		if new_item < 0 && i != BODY_INDEX:
+			pass
+		else:
+			print("random ", new_item)
+			print($Body_Front.get_child(i).get_child_count())
+			$Body_Front.get_child(i).get_child(new_item).show()
+	
+	# parte trasera
+	#for i : int in range($Body_Back.get_child_count()):
+		#
+		#var new_item = randi_range(-1, $Body_Back.get_child(i).get_child_count())
+		#if new_item < 0:
+			#pass
+		#else:
+			#$Body_Back.get_child(i).get_child(new_item).show()
+	# parte X-ray
+
+
 func _on_is_dragged_button_down() -> void:
 	dragging = true
 	of_set = get_global_mouse_position() - global_position
-
 func _on_is_dragged_button_up() -> void:
 	if in_jail:
 		my_position =  get_parent().get_parent().get_node("Jail").position
@@ -36,11 +62,9 @@ func _on_is_dragged_button_up() -> void:
 func _on_is_dragged_pressed() -> void:
 	front_side = false
 
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Jail"):
 		in_jail = true
 		print("Estoy en la carcer")
-
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	in_jail = false
