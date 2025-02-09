82579917efe6e9388384e78d5370421028dbd8af
DaniPerezUJI <90398305+DaniPerezUJI@users.noreply.github.com>
2025-02-09 13:56:10 +0100

    Impement timer

--- proyecto-godot_animal-mugshot/Scripts/ui.gd
+++ proyecto-godot_animal-mugshot/Scripts/ui.gd
@@ -1,7 +1,8 @@
-extends Control
+extends Node2D
 
-var max_timer_value = 180
+var max_timer_value : int= 180
 var timer_value : int = 0
+const left_space : int = 32
 
 func Set_Timer() -> void:
 	timer_value = max_timer_value
@@ -10,4 +11,11 @@ func Set_Timer() -> void:
 func _on_timer_timeout() -> void:
 	timer_value -= 1
 	$TextureProgressBar.value = timer_value
-	pass # Replace with function body.
+
+func _process(delta: float) -> void:
+	var progress_bar = $TextureProgressBar
+	var progress_ratio = timer_value / (float(max_timer_value) *1.05)  # Ratio entre 0 y 1
+	var start_pos = progress_bar.global_position  # Posición inicial de la barra
+	var end_pos = start_pos + Vector2(left_space, progress_bar.size.y * (1 - progress_ratio))
+
+	$CPUParticles2D.global_position = end_pos  # Mover las partículas
